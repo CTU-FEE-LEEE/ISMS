06e3bc5c5fa2726a6eea4e1afeba592cf204321e
hanuson1 <hanuson1@fel.cvut.cz>
2018-04-23 15:05:39 +0200

    fsycnc() added

--- SW/meteo.py
+++ SW/meteo.py
@@ -67,6 +67,7 @@ def saveFile(filename,lst):
         f.write("%d;%0.1f;%0.1f;%0.1f;%0.1f;%0.1f;%0.1f;%0.1f;%0.1f;%0.1f;%0.1f\n"
             %(time.time(),lst[0],lst[1],lst[2],lst[3],lst[4],lst[5],lst[6],lst[7],lst[8],lst[9]))
         f.flush()
+        os.fsync(f.fileno())
     f.close()
     
     
