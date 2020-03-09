91a8a70f9dfd81b0614fcf7bac0aa457fd5e023e
hanuson1 <hanuson1@fel.cvut.cz>
2020-03-09 13:12:29 +0100

    Inf. loop reading

--- SW/modbus_test.py
+++ SW/modbus_test.py
@@ -13,20 +13,26 @@ instrument.serial.timeout  = 0.5   # seconds
 instrument.address = 0x1E    # this is the slave address number
 instrument.mode = minimalmodbus.MODE_RTU   # rtu or ascii mode
 
-instrument.write_register(0x01, 0x1F, 0) # Registernumber, value, number of decimals for storage
+while True:
+    try:
+        instrument.write_register(0x01, 0x1F, 0) # Registernumber, value, number of decimals for storage
 
-time.sleep(0.5)
+        time.sleep(0.5)
 
-## Read temperature (PV = ProcessValue) ##
-temperature = instrument.read_float(0x53, 3, 2) # Registernumber, number of decimals
-print temperature
-conductivity = instrument.read_float(0x55, 3, 2) # Registernumber, number of decimals
-print conductivity
-salinity = instrument.read_float(0x57, 3, 2) # Registernumber, number of decimals
-print salinity
-tds_kcl = instrument.read_float(0x59, 3, 2) # Registernumber, number of decimals
-print tds_kcl
+        ## Read temperature (PV = ProcessValue) ##
+        temperature = instrument.read_float(0x53, 3, 2) # Registernumber, number of decimals
+        print temperature
+        conductivity = instrument.read_float(0x55, 3, 2) # Registernumber, number of decimals
+        print conductivity
+        salinity = instrument.read_float(0x57, 3, 2) # Registernumber, number of decimals
+        print salinity
+        tds_kcl = instrument.read_float(0x59, 3, 2) # Registernumber, number of decimals
+        print tds_kcl
+
+        time.sleep(2)
+
+    except Exception as e:
+        print e
 
 
 ## Change temperature setpoint (SP) ##
-
