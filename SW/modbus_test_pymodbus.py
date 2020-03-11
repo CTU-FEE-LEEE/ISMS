ba7db56fd7b77fc1f0a409c8cae19df5ccc61d32
hanuson1 <hanuson1@fel.cvut.cz>
2020-03-11 14:10:04 +0100

    Update modbus_test_pymodbus.py

--- SW/modbus_test_pymodbus.py
+++ SW/modbus_test_pymodbus.py
@@ -5,14 +5,19 @@ import struct
 
 UNIT = 0x1
 
-client = ModbusClient(method='rtu', port='/dev/ttyUSB0', timeout = 0.5, stopbits = 2, bytesize = 8, parity = 'N')
+client = ModbusClient(method='rtu', port='/dev/ttyUSB0', baudrate=9600, stopbits = 2, bytesize = 8, parity = 'N', timeout = 0.5)
 client.connect()
 
 UNIT = 0x1E
 rq = client.write_register(address = 0x01, value = 0x1F, unit = UNIT)
+print(rq)
+
 time.sleep(0.5)
 
 result0 = client.read_holding_registers(address = 0x53, count = 2, unit = UNIT)
 packed_string = struct.pack("HH", *(result0.registers[1],result0.registers[0]))
 value = struct.unpack("f", packed_string)[0]
+
+client.close()
+
 print(value)
