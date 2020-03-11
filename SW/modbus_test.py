792b9e431026e9739f5341d270315c26d97a660e
hanuson1 <hanuson1@fel.cvut.cz>
2020-03-11 12:32:27 +0100

    Update modbus_test.py

--- SW/modbus_test.py
+++ SW/modbus_test.py
@@ -12,6 +12,7 @@ instrument.serial.timeout  = 0.5   # seconds
 
 instrument.address = 0x1E    # this is the slave address number
 instrument.mode = minimalmodbus.MODE_RTU   # rtu or ascii mode
+instrument.close_port_after_each_call = True
 
 for i in range(0,100):
     try:
